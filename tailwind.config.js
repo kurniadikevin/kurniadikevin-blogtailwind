2aeacdab9231b0f5aef61b36ae49b5d5badbe049
kurniadikevin <kurniadikevin@gmail.com>
2023-05-07 12:52:41 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        importYellow:' rgb(252, 211, 77)'
+      },
+    },
+  },
+  plugins: [],
+}
+
